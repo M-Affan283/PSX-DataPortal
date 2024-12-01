1aa8cc4cab61ef9b3538af531188097e9b3a755b
Muhammad Affan <25100283@lums.edu.pk>
2024-12-01 19:24:01 +0500

    Fixed readme and variables.tf

--- TerraformDeployment/Main/variables.tf
+++ TerraformDeployment/Main/variables.tf
@@ -4,11 +4,11 @@
 variable "fastapi_image" {
     description = "FastAPI Docker image"
     type = string
-    default = "241533118783.dkr.ecr.us-east-1.amazonaws.com/fastapiserver:latest"
+    default = "<your-fastapi-image>"
 }
 
 variable "sns_email" {
     description = "Email to send SNS notifications"
     type = string
-    default = "25100283@lums.edu.pk"
+    default = "<your-email>"
 }
